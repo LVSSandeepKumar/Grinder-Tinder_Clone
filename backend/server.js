ec0d8b3facb9cc91700ac6fa8acefa5eb9f4a20c
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-10-11 13:50:27 +0530

    Database Setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/connectDB.js";
 
 dotenv.config();
 
@@ -7,7 +8,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+//middleware functions
+app.use(express.json()); //middleware to parse incoming requests with json payloads
+
 //Listen to the application on the assigned port
 app.listen(PORT, () => {
     console.log("Server running at", PORT);
+    connectDB();
 })
