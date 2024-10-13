0f65adf438ae4625e8bdd4a9b34acc5d5ffd300a
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-10-13 21:20:27 +0530

    Get conversation logic in backend

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -17,4 +17,24 @@ export const sendMessage = async(req,res) => {
         console.log("Error in sendMessage controller:", error);
         res.status(500).json({ message: "Internal Server Error" });
     }
-}
+};
+
+export const getConversation = async(req,res) => {
+    try {
+        //Read the userId with whom we want to retrieve our conversation
+        const {userId} = req.params;
+        //Find all messages including both users
+        const messages = await Message.find({
+            $or: [
+                {sender: req.user.id, receiver: userId},
+                {sender: userId, receiver: req.user.id}
+            ]
+        }).sort("createdAt");
+        //Send the messages in response to frontend
+        res.status(200).json({messages});
+    } catch (error) {
+        //Error Handling
+        console.log("Error in getConversation controller:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { sendMessage } from "../controllers/message.controller.js";
+import { getConversation, sendMessage } from "../controllers/message.controller.js";
 
 //Setup Express Router
 const router = Router();
 
 //Use authentication middleware to protect routes from unauthorized access
 router.use(protectRoute);
+
 //Routes for various actions
 router.post("/send", sendMessage);
+router.get("/conversation/:userId", getConversation);
 
 export default router;
