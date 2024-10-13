fa8171747f53f16d8e5df3c095ee5812bd3b33eb
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-10-13 20:24:41 +0530

    Get user profiles logic in backend

--- backend/controllers/match.controller.js
+++ backend/controllers/match.controller.js
@@ -12,4 +12,27 @@ export const getMatches = async(req,res) => {
         console.log("Error in getMatches controller:", error);
         res.status(500).json({ message: "Internal Server Error" });
     }
+};
+
+export const getUserProfiles = async(req,res) => {
+    try {
+        //Get the current user by using userId in req
+        const currentUser = await User.findById(req.user._id);
+        //Find the user profiles with certain filters
+        const users = await User.find({
+            $and: [
+                {_id: {$ne: currentUser.id}},
+                {_id: {$nin: currentUser.likes}},
+                {_id: {$nin: currentUser.disikes}},
+                {_id: {$nin: currentUser.matches}},
+                {gender: currentUser.gender === "male" ? {$in: ["female"]} : {$in: ["male"]}}
+            ]
+        });
+        //Send the user doc as a response to frontend
+        res.status(200).json({users});
+    } catch (error) {
+        //Error Handling
+        console.log("Error in getUserProfiles controller:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 }
--- backend/routes/match.route.js
+++ backend/routes/match.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { getMatches } from "../controllers/match.controller.js";
+import { getMatches, getUserProfiles } from "../controllers/match.controller.js";
 
 //Setup Express Router
 const router = Router();
 
 //Routes for various actions
-router.get("/", protectRoute, getMatches)
+router.get("/", protectRoute, getMatches);
+router.get("/user-profiles", protectRoute, getUserProfiles);
 
 export default router;
