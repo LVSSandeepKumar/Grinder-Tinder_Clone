00e69d4b83fc5b5452a0a321756447d8160f1e80
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-10-14 12:56:12 +0530

    Login Page UI

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,7 +1,35 @@
-const AuthPage = () => {
-  return (
-    <div>AuthPage</div>
-  )
-}
+import { useState } from "react";
+import LoginForm from "../components/LoginForm";
+import SignupForm from "../components/SignupForm";
 
-export default AuthPage
+const AuthPage = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-red-500 to-pink-500 p-4">
+      <div className="w-full max-w-md">
+        <h2 className="text-center text-3xl text-white font-extrabold mb-8">
+          {isLogin ? "Sign in to Grinder account" : "Create new Grinder account"}
+        </h2>
+
+        <div className="bg-white shadow-xl rounded-lg p-8">
+          {isLogin ? <LoginForm /> : <SignupForm />}
+
+          <div className="mt-8 text-center">
+            <p className="text-sm text-gray-600">
+              {isLogin ? "New to Grinder?" : "Already have an account?"}
+            </p>
+            <button
+            onClick={() => setIsLogin(prevLogin => !prevLogin)}
+            className="mt-2 text-red-600 hover:text-red-800 font-medium transition-colors duration-300"
+            >
+              {isLogin ? "Create a new account" : "Sign in to your account"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthPage;
